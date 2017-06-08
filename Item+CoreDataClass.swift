0195655044566065e3e35f059c86f9ad5fc18706
pegasusCK <cky1026@gmail.com>
2017-06-08 15:54:29 -0700

    Lesson 104 Creating a Data Model

---
+++ Item+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Item+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Chihkao Yu on 6/8/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Item: NSManagedObject {
+
+}
---
+++ ItemType+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ItemType+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by Chihkao Yu on 6/8/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class ItemType: NSManagedObject {
+
+}
