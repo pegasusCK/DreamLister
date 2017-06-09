7c7fb100298b7651b8f94415f6cd17367657f9f4
pegasusCK <cky1026@gmail.com>
2017-06-09 14:32:38 -0700

    Lesson 105 Designing the Views

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Chihkao Yu on 6/8/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Item+CoreDataClass.swift
+++ Item+CoreDataClass.swift
@@ -11,5 +11,11 @@ import CoreData
 
 
 public class Item: NSManagedObject {
-
+    
+    //when item is created from entity, then perform this function
+    public override func awakeFromInsert() {
+        
+        super.awakeFromInsert()
+        self.created = NSDate() //assign current date (created date) to created attribute
+    }
 }
